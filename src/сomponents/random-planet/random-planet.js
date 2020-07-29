4b0e908a6b1dc1132e9aee9a49987fd0cec2fe0a
Oleksandr <denysiuk.oleksandr1997@gmail.com>
2020-07-29 17:17:25 +0200

    Use prop types

--- src/сomponents/random-planet/random-planet.js
+++ src/сomponents/random-planet/random-planet.js
@@ -6,6 +6,14 @@ import ErrorIndicator from "../error-indicator";
 
 export default class RandomPlanet extends Component {
 
+    static defaultProps = {
+        updateInterval: 10000
+    };
+
+    static propTypes = {
+        updateInterval: PropTypes.number
+    };
+
     swapiService = new SwapiService();
 
     state = {
